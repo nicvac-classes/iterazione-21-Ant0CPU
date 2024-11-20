7314cd0a4a38e3f77c95673186231450870565b1
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-11-20 16:48:40 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,43 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int max, persone, eta, s;
+    float posti, somma;
+
+    max=30;
+    somma=0;
+
+    while(somma<max)
+    {
+        cout<<"Inserire il numero di persone"<<endl;
+        cin>>persone;
+
+        cout<<"Inserire l'età"<<endl;
+        cin>>eta;
+
+        if (eta>=18)
+        {
+            posti=persone;
+        }
+        else
+        {
+            posti=persone/2;
+        }
+        somma=somma+posti;
+    }
+
+    if (somma>max)
+    {
+        cout<<"La giostra non può partire"<<endl;
+        
+        s=somma-max;
+
+        cout<<"Devono scendere "&s& "persone"<<endl;
+    }
+    else
+    {
+        cout<<"La giostra può partire"<<endl;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
